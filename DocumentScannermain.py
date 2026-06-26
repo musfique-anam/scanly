b36d53217967e0571db9d08ded111da06f58a49e
musfique-anam <m.musfique2002@gmail.com>
2026-06-26 23:08:20 +0600

    working

--- DocumentScannermain.py
+++ DocumentScannermain.py
@@ -9,3 +9,16 @@ cap = cv2.VideoCapture(1)
 cap.set(10, 160)
 heightImg = 640
 widthImg = 480
+
+
+utils.initializeTrackbars()
+count = 0
+
+
+while True:
+
+    imgBlank = np.zeros((heightImg, widthImg, 3), np.uint8)
+
+    if webCamFeed:success, img = cap.read()
+    else:img = cv2.imread(pathImage)
+    img = cv2.resize(img, (widthImg, heightImg))
